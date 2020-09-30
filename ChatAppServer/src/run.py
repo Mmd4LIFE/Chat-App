6924f73c85a397cee45a8c26d47cb8095fe73342
Mmd4LIFE <svmykel@gmail.com>
2020-09-30 16:09:01 +0330

    init

---
+++ ChatAppServer/src/run.py
@@ -0,0 +1,14 @@
+
+
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    try:
+        main()
+    except KeyboardInterrupt:
+        SystemExit, print("[-] somebody killed this server")
+    
