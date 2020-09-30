7413b1772cb855777e92b966274205bb48bb8f02
Mmd4LIFE <svmykel@gmail.com>
2020-09-30 17:42:15 +0330

    1st edit

---
+++ ChatAppServer/src/commons/constants/paths.py
@@ -0,0 +1,6 @@
+"""
+    created at sep 30/2020 by Mmd4LIFE
+    - all constant paths
+"""
+
+APP_SETTING_PATH = "appsettings.json"
--- ChatAppServer/src/utils/config_manager.py
+++ ChatAppServer/src/utils/config_manager.py
@@ -3,9 +3,21 @@
     - this package handle app configs
 """
 from json import loads as json_loads
+
 from easydict import EasyDict as edict
+
+from ChatAppServer.src.commons.constants.paths import APP_SETTING_PATH
 
 class ConfigManager:
 
     def __init__(self):
-        self.config_path = "appsetings.json"
+        self.config_path = APP_SETTING_PATH
+
+    def __read_config__(self):
+        with open(self.config_path, 'r') as file:
+            configs = json_loads(file.read().strip("\n"))
+        return configs
+
+    def get(self):
+        dict_e = edict(self.__read_config__())
+        return dict_e
