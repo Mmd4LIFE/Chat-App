c62a9b1a41d953980cc5dce24b1b6ae42dc4bf0a
Mmd4LIFE <svmykel@gmail.com>
2020-10-01 16:51:22 +0330

    add color to code

---
+++ ChatAppCLient/src/commons/constants/colors.py
@@ -0,0 +1,23 @@
+"""
+    created at oct 01/2020 by Mmd4LIFE
+    - this package create console color
+"""
+
+from colorama import Fore, Back, init as colorama_init
+
+class Color:
+    colorama_init(autoreset=True)
+
+    FORE_GREEN = Fore.GREEN
+    FORE_RED = Fore.RED
+    FORE_YELLOW = Fore.YELLOW
+    FORE_CYAN = Fore.CYAN
+    FORE_BLUE = Fore.BLUE
+    FORE_WHITE = Fore.WHITE
+
+    BACK_GREEN = Back.GREEN
+    BACK_RED = Back.RED
+    BACK_YELLOW = Back.YELLOW
+    BACK_CYAN = Back.CYAN
+    BACK_BLUE = Back.BLUE
+    BACK_WHITE = Back.WHITE
---
+++ ChatAppCLient/src/run.py
@@ -0,0 +1,11 @@
+"""
+    created at oct 01/2020 by Mmd4LIFE
+    - this package is entry point of applocation
+"""
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
